ace5a209bc554d21cd6d5377edcc703d58cbd234
psvoboda1987 <psvoboda1987@gmail.com>
2025-02-25 16:05:28 +0100

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,454 @@
+export default class Controller {
+    constructor() {
+    }
+
+    fallback() {
+        return {};
+    }
+
+    async animalFacts() {
+        const animal = this.getUrlParameter('animal')
+        const items = await fetch('data/animal_facts.json')
+            .then(reply => reply.json())
+            .then(json => json.data);
+        if (animal) {
+            const filteredItems = items.filter(item => item.animal === animal);
+            const randomIndex = this.getRandomNumber(0, filteredItems.length);
+            return filteredItems[randomIndex];
+        }
+        const randomIndex = this.getRandomNumber(0, items.length);
+        return items[randomIndex];
+    }
+
+    getRandomNumber(min = 0, max = 10) {
+        return (min + Math.floor(Math.random() * (max - min + 1)));
+    }
+
+    vowelCount() {
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+        let foundVowels = 0;
+        const string = this.getUrlParameter('string');
+        for (const element of string) {
+            if (vowels.includes(element)) {
+                foundVowels++;
+            }
+        }
+        return foundVowels;
+    }
+
+    async chuckNorrisJokes() {
+        const items = await fetch('data/chuck_jokes.json')
+            .then(reply => reply.json())
+            .then(json => json.data);
+        const randomIndex = this.getRandomNumber(0, items.length);
+        return items[randomIndex];
+    }
+
+    async cities() {
+        const zip = this.getUrlParameter('zip');
+        const items = await fetch('data/post_codes.json')
+            .then(reply => reply.json())
+            .then(json => json.data);
+        return items.filter(item => item.zip === zip);
+    }
+
+    async counties() {
+        const county = decodeURI(this.getUrlParameter('county'));
+        const items = await fetch('data/post_codes.json')
+            .then(reply => reply.json())
+            .then(json => json.data);
+        return items.filter(item => item.county === county);
+    }
+
+    async countries() {
+        const country = this.getUrlParameter('country');
+        const items = await fetch('data/countries.json')
+            .then(reply => reply.json())
+            .then(json => json.data);
+        let filteredItems = items.filter(item => item.country === country);
+        return {
+            country,
+            capital: filteredItems[0].capital
+        };
+    }
+
+    async postalCodes() {
+        const city = this.getUrlParameter('city');
+        const items = await fetch('data/post_codes.json')
+            .then(reply => reply.json())
+            .then(json => json.data);
+        return items.filter(item => item.city === city);
+    }
+
+    async regions() {
+        const zip = this.getUrlParameter('zip');
+        const items = await fetch('data/post_codes.json')
+            .then(reply => reply.json())
+            .then(json => json.data);
+        let filteredItems = items.filter(item => item.zip === zip)
+        return {
+            zip,
+            region: filteredItems[0].region
+        };
+    }
+
+    uniqueLetters() {
+        const string = this.getUrlParameter('string');
+        let uniqueLetters = [];
+        for (const element of string) {
+            if (!uniqueLetters.includes(element)) {
+                uniqueLetters.push(element);
+            }
+        }
+        return {
+            uniqueLetters,
+            count: uniqueLetters.length
+        };
+    }
+
+    averageWordLength() {
+        const stringParam = this.getUrlParameter('string');
+        const string = decodeURI(stringParam);
+        const wordCount = string.split(' ').length;
+        const textLength = string.length;
+        return (textLength / wordCount).toFixed(2);
+    }
+
+    getMultiples() {
+        const count = parseFloat(this.getUrlParameter('count'));
+        const number = parseFloat(this.getUrlParameter('number'));
+        let multiples = [];
+        let value = 0;
+        for (let i = 0; i <= count; i++) {
+            value += number;
+            multiples.push(value);
+        }
+        return multiples;
+    }
+
+    fizzbuzz() {
+        const number = parseInt(this.getUrlParameter('number'));
+        let result = [];
+        for (let i = 1; i <= number; i++) {
+            result.push(this.getFizzbuzzFromNumber(i));
+        }
+        return result;
+    }
+
+    fibonacciSequence() {
+        const count = parseInt(this.getUrlParameter('number'));
+        let sequence = [];
+        for (let i = 1; i <= count; i++) {
+            sequence.push(this.getFibonacciValue(i));
+        }
+        return sequence;
+    }
+
+    fibonacciValue() {
+        const number = parseInt(this.getUrlParameter('number'));
+        return this.getFibonacciValue(number);
+    }
+
+    getFibonacciValue(number) {
+        if (number <= 0) {
+            return null;
+        }
+        if (number === 1) {
+            return 0;
+        }
+        if (number === 2 || number === 3) {
+            return 1;
+        }
+        let fibValues = [0, 1, 1];
+        for (let i = 3; i < number; i++) {
+            fibValues[i] = fibValues[i - 2] + fibValues[i - 1];
+        }
+
+        return fibValues[number - 1];
+    }
+
+    getFizzbuzzFromNumber(number) {
+        if (number === 0) {
+            return 0;
+        }
+        if (number % 15 === 0) {
+            return 'fizzbuzz';
+        }
+        if (number % 3 === 0) {
+            return 'fizz';
+        }
+        if (number % 5 === 0) {
+            return 'buzz';
+        }
+        return number;
+    }
+
+    isPrimeNumber() {
+        const number = parseInt(this.getUrlParameter('number'));
+        return {
+            number,
+            isPrimeNumber: this.isNumberPrime(number)
+        };
+    }
+
+    isNumberPrime(number) {
+        return this.getDividers(number).length <= 2;
+    }
+
+    getDividers(number) {
+        let dividers = [];
+        let divider = number;
+        while (divider > 0) {
+            if (number % divider === 0) {
+                dividers.push(divider);
+            }
+            divider--;
+        }
+        return dividers;
+    }
+
+    primeNumbers() {
+        const number = parseInt(this.getUrlParameter('number'));
+        const from = parseInt(this.getUrlParameter('from'));
+        let i = from - 1;
+        let primes = [];
+        while (primes.length !== number) {
+            if (this.isNumberPrime(i)) {
+                primes.push(i);
+            }
+            i++;
+        }
+        return primes;
+    }
+
+    sum() {
+        let numbers = this.getUrlParameter('numbers').split(',')
+            .map(item => parseInt(item));
+        let settings = this.getUrlParameter('settings');
+        let resultNumbers = [];
+        if (settings === null || settings === 'all') {
+            resultNumbers = this.getEvenAndOdd(numbers);
+        }
+        if (settings === 'even') {
+            resultNumbers = this.getItemsEven(numbers);
+        }
+        if (settings === 'odd') {
+            resultNumbers = this.getItemsOdd(numbers);
+        }
+        return {
+            numbers: resultNumbers,
+            sum: resultNumbers.reduce((total, num) => total + num)
+        };
+    }
+
+    getEvenAndOdd(numbersParam) {
+        return numbersParam.filter(item => item % 1 === 0);
+    }
+
+    getItemsEven(numbersParam) {
+        return numbersParam.filter(item => item % 2 === 0);
+    }
+
+    getItemsOdd(numbersParam) {
+        return numbersParam.filter(item => item % 2 !== 0);
+    }
+
+    factors() {
+        const number = parseInt(this.getUrlParameter('number'));
+        let factors = [];
+        for (let i = 0; i <= number; i++) {
+            if (number % i !== 0) {
+                continue;
+            }
+            factors.push(i);
+        }
+        return factors;
+    }
+
+    isPalindrome() {
+        let word = this.getUrlParameter('word')
+        return word === word.split('').reverse().join('');
+    }
+
+    romanNumberToInteger() {
+        const romanNumberMapping = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        };
+        const romanNumberSubtractMapping = {
+            // -1
+            'IV': 4,
+            'IX': 9,
+            'IL': 49,
+            'IC': 99,
+            'ID': 499,
+            'IM': 999,
+            // -5
+            'VL': 45,
+            'VC': 95,
+            'VD': 495,
+            'VM': 995,
+            // -10
+            'XL': 40,
+            'XC': 90,
+            'XD': 490,
+            'XM': 990,
+            // -50
+            'LD': 450,
+            'LM': 950,
+            // -100
+            'CD': 400,
+            'CM': 900,
+        };
+
+        const string = this.getUrlParameter('roman');
+        let strlen = string.length;
+        let int = 0;
+        for (let i = 0; i < strlen; i++) {
+            let index = i;
+            let letter = string[index].toUpperCase();
+            let nextLetter = (string[++index] ?? '').toUpperCase();
+            let subtractIndex = (letter + nextLetter);
+            let subtractMappingElement = romanNumberSubtractMapping[subtractIndex];
+            let mappingElement = romanNumberMapping[letter];
+            if (subtractMappingElement !== undefined) {
+                int += subtractMappingElement;
+                i++;
+            } else if (mappingElement !== undefined) {
+                int += mappingElement;
+            }
+        }
+        return int;
+    }
+
+    getUrlVariables() {
+        let variables = {};
+        window.location.href.replace(
+            /[?&]+([^=&]+)=([^&]*)/gi,
+            function (m, key, value) {
+                variables[key] = value;
+            }
+        );
+        return variables;
+    }
+
+    getUrlParameter(parameter) {
+        if (window.location.href.indexOf(parameter) === 0) return '';
+        return this.getUrlVariables()[parameter];
+    }
+
+    convertJsonToXml() {
+        let json = this.getUrlParameter('json');
+        let object = JSON.parse(decodeURI(json));
+        return this.JSONtoXML(object);
+    }
+
+    JSONtoXML(obj) {
+        let xml = '';
+        for (let prop in obj) {
+            xml += obj[prop] instanceof Array ? '' : '<' + prop + '>';
+            if (obj[prop] instanceof Array) {
+                for (let array in obj[prop]) {
+                    xml += '\n<' + prop + '>\n';
+                    xml += this.JSONtoXML(new Object(obj[prop][array]));
+                    xml += '</' + prop + '>';
+                }
+            } else if (typeof obj[prop] == 'object') {
+                xml += this.JSONtoXML(new Object(obj[prop]));
+            } else {
+                xml += obj[prop];
+            }
+            xml += obj[prop] instanceof Array ? '' : '</' + prop + '>\n';
+        }
+        xml = xml.replace(/<\/?[0-9]+>/g, '');
+        return xml;
+    }
+
+    validateCompanyNumber() {
+        let cin = this.getUrlParameter('number')
+            .replace(/#\s+#/, '');
+        if (cin.match(/\d{8}/) === null) {
+            return false;
+        }
+        let check = 0;
+        for (let i = 0; i < 7; i++) {
+            check += parseInt(cin[i]) * (8 - i);
+        }
+        check %= 11;
+        if (check === 0) {
+            return parseInt(cin[7]) === 1;
+        }
+        if (check === 1) {
+            return parseInt(cin[7]) === 0;
+        }
+        return parseInt(cin[7]) === 11 - check;
+    }
+
+    validateBirthNumber() {
+        let pin = this.getUrlParameter('number');
+        let matches = pin.match(/(\d\d)(\d\d)(\d\d)[ /]*(\d\d\d)(\d?)/);
+        if (matches === null) {
+            return false;
+        }
+        let [, year, month, day, ext, c] = matches;
+        if (c === '') {
+            year = parseInt(year);
+            year += year < 54 ? 1900 : 1800;
+            return this.isDateValid(this.getMonth(month, year), day, year);
+        }
+        let controlEntity = (year + month + day + ext) % 11;
+        if (controlEntity === 10) {
+            controlEntity = 0;
+        }
+        if (controlEntity !== parseInt(c)) {
+            return false;
+        }
+        year = parseInt(year);
+        year += year < 54 ? 2000 : 1900;
+        return this.isDateValid(this.getMonth(month, year), day, year);
+    }
+
+    isDateValid(month, day, year) {
+        month = parseInt(month);
+        day = parseInt(day);
+        year = parseInt(year);
+
+        return (0 <= month)
+            && (month <= 11)
+            && (0 < year)
+            && (year < 32768)
+            && (0 < day)
+            && (day <= (new Date(year, month, 0)).getDate())
+    }
+
+    getMonth(month, year) {
+        if (month === null) {
+            month = this.getUrlParameter('month');
+        }
+        if (year === null) {
+            year = this.getUrlParameter('year');
+        }
+        month = parseInt(month);
+        year = parseInt(year);
+
+        // k měsíci může být připočteno 20, 50 nebo 70
+        if (month > 70 && year > 2003) {
+            month -= 70;
+            return month;
+        }
+        if (month > 50) {
+            month -= 50;
+            return month;
+        }
+        if (month > 20 && year > 2003) {
+            month -= 20;
+            return month;
+        }
+        return month;
+    }
+}
---
+++ moon_phase.js
@@ -0,0 +1,443 @@
+export default class MoonPhase {
+    constructor(date = null) {
+        date = date === null ? new Date().getTime() : date.getTime();
+        this.timestamp = date;
+
+        // Astronomical constants. 1980 January 0.0
+        let epoch = 2_444_238.5;
+        // Constants defining the Sun's apparent orbit
+        // Ecliptic longitude of the Sun at epoch 1980.0
+        let elonge = 278.833540;
+
+        // Ecliptic longitude of the Sun at perigee
+        let elongp = 282.596403;
+
+        // Eccentricity of Earth's orbit
+        let eccent = 0.016718;
+
+        // Semi-major axis of Earth's orbit, km
+        let sunsmax = 1.495985e8;
+
+        // Sun's angular size, degrees, at semi-major axis distance
+        let sunangsiz = 0.533128;
+
+        // Elements of the Moon's orbit, epoch 1980.0
+
+        // Moon's mean longitude at the epoch
+        let mmlong = 64.975464;
+
+        // Mean longitude of the perigee at the epoch
+        let mmlongp = 349.383063;
+
+        // Eccentricity of the Moon's orbit
+        let mecc = 0.054900;
+
+        // Moon's angular size at distance a from Earth
+        let mangsiz = 0.5181;
+
+        // Semi-major axis of Moon's orbit in km
+        let msmax = 384401;
+
+        // Synodic month (new Moon to new Moon)
+        let synmonth = 29.53058868;
+
+        this.synmonth = synmonth;
+
+        // date is coming in as a UNIX timstamp, so convert it to Julian
+        date = date / 86400 + 2_440_587.5;
+
+        // Calculation of the Sun's position
+
+        // Date within epoch
+        let day = date - epoch;
+
+        // Mean anomaly of the Sun
+        let n = this.fixAngle((360 / 365.2422) * day);
+
+        // Convert from perigee co-ordinates to epoch 1980.0
+        let m = this.fixAngle(n + elonge - elongp);
+
+        // Solve equation of Kepler
+        let ec = this.kepler(m, eccent);
+        ec = Math.sqrt((1 + eccent) / (1 - eccent)) * Math.tan(ec / 2);
+
+        // True anomaly
+        ec = 2 * this.radiansToDegrees(Math.atan(ec));
+
+        // Sun's geocentric ecliptic longitude
+        let lambdaSun = this.fixAngle(ec + elongp);
+
+        // Orbital distance factor
+        let f = ((1 + eccent * Math.cos(this.degreesToRadians(ec))) / (1 - eccent * eccent));
+
+        // Distance to Sun in km
+        let sunDist = sunsmax / f;
+
+        // Sun's angular size in degrees
+        let sunAng = f * sunangsiz;
+
+        // Calculation of the Moon's position
+
+        // Moon's mean longitude
+        let ml = this.fixAngle(13.1763966 * day + mmlong);
+
+        // Moon's mean anomaly
+        let mm = this.fixAngle(ml - 0.1114041 * day - mmlongp);
+
+        let evection = 1.2739 * Math.sin(this.degreesToRadians(2 * (ml - lambdaSun) - mm));
+
+        let annualEquation = 0.1858 * Math.sin(this.degreesToRadians(m));
+
+        // Correction term
+        let a3 = 0.37 * Math.sin(this.degreesToRadians(m));
+
+        // Corrected anomaly
+        let mmp = mm + evection - annualEquation - a3;
+
+        // Correction for the equation of the centre
+        let mEc = 6.2886 * Math.sin(this.degreesToRadians(mmp));
+
+        // Another correction term
+        let a4 = 0.214 * Math.sin(this.degreesToRadians(2 * mmp));
+
+        // Corrected longitude
+        let lP = ml + evection + mEc - annualEquation + a4;
+
+        let variation = 0.6583 * Math.sin(this.degreesToRadians(2 * (lP - lambdaSun)));
+
+        // True longitude
+        let lPP = lP + variation;
+
+        // Calculation of the phase of the Moon
+
+        // Age of the Moon in degrees
+        let moonAge = lPP - lambdaSun;
+
+        // Phase of the Moon
+        let moonPhase = (1 - Math.cos(this.degreesToRadians(moonAge))) / 2;
+
+        // Distance of moon from the centre of the Earth
+        let moonDist = (msmax * (1 - mecc * mecc)) / (1 + mecc * Math.cos(this.degreesToRadians(mmp + mEc)));
+
+        let moonDFrac = moonDist / msmax;
+
+        // Moon's angular diameter
+        let moonAng = mangsiz / moonDFrac;
+
+        // Store results
+
+        // Phase (0 to 1)
+        this.phase = this.fixAngle(moonAge) / 360;
+
+        // Illuminated fraction (0 to 1)
+        this.illumination = moonPhase;
+
+        // Age of moon (days)
+        this.age = synmonth * this.phase;
+
+        // Distance (kilometres)
+        this.distance = moonDist;
+
+        // Angular diameter (degrees)
+        this.diameter = moonAng;
+
+        // Age of the Moon in degrees
+        this.ageDegrees = moonAge;
+
+        // Distance to Sun (kilometres)
+        this.sunDistance = sunDist;
+
+        // Sun's angular diameter (degrees)
+        this.sunDiameter = sunAng;
+    }
+
+    degreesToRadians(degrees) {
+        return degrees * (Math.PI / 180);
+    }
+
+    radiansToDegrees(radians) {
+        return radians * (180 / Math.PI);
+    }
+
+    fixAngle(angle) {
+        return angle - 360 * Math.floor(angle / 360);
+    }
+
+    kepler(m, ecc) {
+        // 1E-6
+        let epsilon = 0.000001;
+        let e = m = this.degreesToRadians(m);
+
+        while (true) {
+            let delta = e - ecc * Math.sin(e) - m;
+            if (Math.abs(delta) > epsilon) {
+                return e;
+            }
+            e -= delta / (1 - ecc * Math.cos(e));
+        }
+    }
+
+    /**
+     * Calculates time  of the mean new Moon for a given base date.
+     * This argument K to this function is the precomputed synodic month index, given by:
+     * K = (year - 1900) * 12.3685
+     * where year is expressed as a year and fractional year.
+     */
+    meanPhase(date, k) {
+        // Time in Julian centuries from 1900 January 0.5
+        let jt = (date - 2_415_020.0) / 36525;
+        let t2 = jt * jt;
+        let t3 = t2 * jt;
+
+        return 2_415_020.75933 + this.synmonth * k
+            + 0.0001178 * t2
+            - 0.000000155 * t3
+            + 0.00033 * Math.sin(this.degreesToRadians(166.56 + 132.87 * jt - 0.009173 * t2));
+    }
+
+    /**
+     * Given a K value used to determine the mean phase of the new moon and a
+     * phase selector (0.0, 0.25, 0.5, 0.75), obtain the true, corrected phase time.
+     */
+    truePhase(k, phase) {
+        let apcor = false;
+
+        // Add phase to new moon time
+        k += phase;
+
+        // Time in Julian centuries from 1900 January 0.5
+        let t = k / 1236.85;
+
+        // Square for frequent use
+        let t2 = t * t;
+
+        // Cube for frequent use
+        let t3 = t2 * t;
+
+        // Mean time of phase
+        let pt = 2_415_020.75933
+            + this.synmonth * k
+            + 0.0001178 * t2
+            - 0.000000155 * t3
+            + 0.00033 * Math.sin(this.degreesToRadians(166.56 + 132.87 * t - 0.009173 * t2));
+
+// Sun's mean anomaly
+        let m = 359.2242 + 29.10535608 * k - 0.0000333 * t2 - 0.00000347 * t3;
+
+// Moon's mean anomaly
+        let mprime = 306.0253 + 385.81691806 * k + 0.0107306 * t2 + 0.00001236 * t3;
+
+// Moon's argument of latitude
+        let f = 21.2964 + 390.67050646 * k - 0.0016528 * t2 - 0.00000239 * t3;
+
+        if (phase < 0.01 || Math.abs(phase - 0.5) < 0.01) {
+            // Corrections for New and Full Moon
+            pt += (0.1734 - 0.000393 * t) * Math.sin(this.degreesToRadians(m))
+                + 0.0021 * Math.sin(this.degreesToRadians(2 * m))
+                - 0.4068 * Math.sin(this.degreesToRadians(mprime))
+                + 0.0161 * Math.sin(this.degreesToRadians(2 * mprime))
+                - 0.0004 * Math.sin(this.degreesToRadians(3 * mprime))
+                + 0.0104 * Math.sin(this.degreesToRadians(2 * f))
+                - 0.0051 * Math.sin(this.degreesToRadians(m + mprime))
+                - 0.0074 * Math.sin(this.degreesToRadians(m - mprime))
+                + 0.0004 * Math.sin(this.degreesToRadians(2 * f + m))
+                - 0.0004 * Math.sin(this.degreesToRadians(2 * f - m))
+                - 0.0006 * Math.sin(this.degreesToRadians(2 * f + mprime))
+                + 0.0010 * Math.sin(this.degreesToRadians(2 * f - mprime))
+                + 0.0005 * Math.sin(this.degreesToRadians(m + 2 * mprime));
+
+            apcor = true;
+        } else if (Math.abs(phase - 0.25) < 0.01 || Math.abs(phase - 0.75) < 0.01) {
+            pt += (0.1721 - 0.0004 * t) * Math.sin(this.degreesToRadians(m))
+                + 0.0021 * Math.sin(this.degreesToRadians(2 * m))
+                - 0.6280 * Math.sin(this.degreesToRadians(mprime))
+                + 0.0089 * Math.sin(this.degreesToRadians(2 * mprime))
+                - 0.0004 * Math.sin(this.degreesToRadians(3 * mprime))
+                + 0.0079 * Math.sin(this.degreesToRadians(2 * f))
+                - 0.0119 * Math.sin(this.degreesToRadians(m + mprime))
+                - 0.0047 * Math.sin(this.degreesToRadians(m - mprime))
+                + 0.0003 * Math.sin(this.degreesToRadians(2 * f + m))
+                - 0.0004 * Math.sin(this.degreesToRadians(2 * f - m))
+                - 0.0006 * Math.sin(this.degreesToRadians(2 * f + mprime))
+                + 0.0021 * Math.sin(this.degreesToRadians(2 * f - mprime))
+                + 0.0003 * Math.sin(this.degreesToRadians(m + 2 * mprime))
+                + 0.0004 * Math.sin(this.degreesToRadians(m - 2 * mprime))
+                - 0.0003 * Math.sin(this.degreesToRadians(2 * m + mprime));
+
+            // First and last quarter corrections
+            if (phase < 0.5) {
+                pt += 0.0028 - 0.0004 * Math.cos(this.degreesToRadians(m)) + 0.0003 * Math.cos(this.degreesToRadians(mprime));
+            } else {
+                pt += -0.0028 + 0.0004 * Math.cos(this.degreesToRadians(m)) - 0.0003 * Math.cos(this.degreesToRadians(mprime));
+            }
+
+            apcor = true;
+        }
+
+        return apcor ? pt : null;
+    }
+
+    /**
+     * Find time of phases of the moon which surround the current date. Five phases are found, starting and
+     * ending with the new moons which bound the current lunation.
+     */
+    phaseHunt() {
+        let sdate = this.getJulianFromUTC(this.timestamp);
+        let adate = sdate - 45;
+        let ats = this.timestamp - 86400 * 45;
+        let date = new Date(ats);
+        let yy = date.getFullYear();
+        let mm = date.getMonth() + 1;
+
+        let k1 = Math.floor((yy + ((mm - 1) * (1 / 12)) - 1900) * 12.3685);
+        let nt1 = this.meanPhase(() => adate, k1);
+        adate = nt1;
+        let k2;
+        let x = 0
+        while (x < 1000) {
+            adate += this.synmonth;
+            k2 = k1 + 1;
+            let nt2 = this.meanPhase(() => adate, k2);
+
+            // If nt2 is close to sdate, then mean phase isn't good enough, we have to be more accurate
+            if (Math.abs(nt2 - sdate) < 0.75) {
+                nt2 = this.truePhase(k2, 0.0);
+            }
+
+            if (nt1 <= sdate && nt2 > sdate) {
+                break;
+            }
+
+            nt1 = nt2;
+            k1 = k2;
+            x++;
+        }
+
+        // Results in Julian dates
+        let dates = [
+            this.truePhase(k1, 0.0),
+            this.truePhase(k1, 0.25),
+            this.truePhase(k1, 0.5),
+            this.truePhase(k1, 0.75),
+            this.truePhase(k2, 0.0),
+            this.truePhase(k2, 0.25),
+            this.truePhase(k2, 0.5),
+            this.truePhase(k2, 0.75),
+        ];
+
+        this.quarters = [];
+
+        for (let jdate of dates) {
+            // Convert to UNIX time
+            this.quarters.push((jdate - 2_440_587.5) * 86400);
+        }
+    }
+
+    /**
+     * UTC to Julian
+     */
+    getJulianFromUTC(timestamp) {
+        return timestamp / 86400 + 2_440_587.5;
+    }
+
+    getPhase() {
+        return this.phase;
+    }
+
+    getIllumination() {
+        return this.illumination;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    getDistance() {
+        return this.distance;
+    }
+
+    getDiameter() {
+        return this.diameter;
+    }
+
+    getSunDistance() {
+        return this.sunDistance;
+    }
+
+    getSunDiameter() {
+        return this.sunDiameter;
+    }
+
+    /**
+     * Get moon phase data
+     */
+    getPhaseByName(name) {
+        let phases = [
+            'new_moon',
+            'first_quarter',
+            'full_moon',
+            'last_quarter',
+            'next_new_moon',
+            'next_first_quarter',
+            'next_full_moon',
+            'next_last_quarter',
+        ];
+
+        if (typeof this.quarters === 'undefined') {
+            this.phaseHunt();
+        }
+        return this.quarters[phases.reverse().indexOf(name)] ?? null;
+    }
+
+    /**
+     * Get current phase name. There are eight phases, evenly split.
+     * A "New Moon" occupies the 1/16th phases either side of phase = 0, and the rest follow from that.
+     */
+    getPhaseName() {
+        let names = [
+            'New Moon',
+            'Waxing Crescent',
+            'First Quarter',
+            'Waxing Gibbous',
+            'Full Moon',
+            'Waning Gibbous',
+            'Third Quarter',
+            'Waning Crescent',
+            'New Moon',
+        ];
+
+        return names[Math.floor((this.phase + 0.0625) * 8)];
+    }
+
+    getPhaseNewMoon() {
+        return this.getPhaseByName('new_moon');
+    }
+
+    getPhaseFirstQuarter() {
+        return this.getPhaseByName('first_quarter');
+    }
+
+    getPhaseFullMoon() {
+        return this.getPhaseByName('full_moon');
+    }
+
+    getPhaseLastQuarter() {
+        return this.getPhaseByName('last_quarter');
+    }
+
+    getPhaseNextNewMoon() {
+        return this.getPhaseByName('next_new_moon');
+    }
+
+    getPhaseNextFirstQuarter() {
+        return this.getPhaseByName('next_first_quarter');
+    }
+
+    getPhaseNextFullMoon() {
+        return this.getPhaseByName('next_full_moon');
+    }
+
+    getPhaseNextLastQuarter() {
+        return this.getPhaseByName('next_last_quarter');
+    }
+}
